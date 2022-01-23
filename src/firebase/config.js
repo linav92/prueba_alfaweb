182f321b6624bd6f4f8e4a907012c5774d28fbf8
Lina Vallejo <linav92@gmail.com>
2022-01-23 19:53:18 -0300

    [main] add config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export default{
+  apiKey: "AIzaSyBpvhlnTsYAJgeBli3ewFQJIxyXIz5HL-o",
+  authDomain: "prueba-desafio-14067.firebaseapp.com",
+  projectId: "prueba-desafio-14067",
+  storageBucket: "prueba-desafio-14067.appspot.com",
+  messagingSenderId: "638795735191",
+  appId: "1:638795735191:web:d7613a2454f72bfe1f09d2"
+}
